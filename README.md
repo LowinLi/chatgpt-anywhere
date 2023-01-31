bbbd3353ceabfb3a8ccf205fa203372125b524f3
lowinli <lowinli@outlook.com>
2023-01-31 20:12:03 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://chatgpt-anywhere.streamlit.app/)
---
+++ src/chatgpt-anywhere/Introduction.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+import openai
+
+
+app_title = os.environ.get("APP_TITLE", "ChatGPT Anywhere in Streamlit App")
+st.title(app_title)
+
+st.markdown("If it is helpful for you, please star this [repository](https://github.com/LowinLi/chatgpt-anywhere).")
